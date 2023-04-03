0cd08ad19e6561d6a20cc857679da35ec78e4251
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-04-03 21:01:09 +0300

    Преобразовал ответ в строку, вывел первые 2 цифры

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let num = 266219;
 
 let mas = [...266219+''].map(Number);
 const res = mas.reduce((acc, rec) => acc * rec);
-let deg = (res ** 3);
-console.log(deg);
+const deg = (res ** 3);
+const str =String(deg)
+console.log(str.substring(0,2));
 
 
 
