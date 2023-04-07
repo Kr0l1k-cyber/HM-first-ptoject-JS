b9c51038c86b5e08f141297e47d1693935c18825
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-04-07 19:55:40 +0300

    Решение через if

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let num = 266219;
+'use strict';
 
-let mas = [...266219+''].map(Number);
-const res = mas.reduce((acc, rec) => acc * rec);
-const deg = (res ** 3);
-const str =String(deg)
-console.log(str.substring(0,2));
+const ru = 'Понедельник, вторник, среда, четверг, пятница, суббота, восскресенье';
+const en = 'Monday, tuesday, wensdey, thursday, friday, saturday';
 
+let lang = 'en';
 
-
+if (lang === 'ru') {
+    console.log(ru);
+} else if (lang === 'en') {
+    console.log(en);
+} else {
+    console.log('Неверное значение переменной lang');
+}
