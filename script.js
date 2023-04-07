b85ec75dd4a7c4cc8db8978e1f29823c6decc92f
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-04-07 20:43:03 +0300

    Решение через многомерный массив

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 'use strict';
 
-const ru = 'Понедельник, вторник, среда, четверг, пятница, суббота, восскресенье';
-const en = 'Monday, tuesday, wensdey, thursday, friday, saturday';
+// const ru = 'Понедельник, вторник, среда, четверг, пятница, суббота, восскресенье';
+// const en = 'Monday, tuesday, wensdey, thursday, friday, saturday';
 
-let lang = 'ru';
+// let lang = 'ru';
 
-if (lang === 'ru') {
-    console.log(ru);
-} else if (lang === 'en') {
-    console.log(en);
-} else {
-    console.log('Неверное значение переменной lang');
-}
+// if (lang === 'ru') {
+//     console.log(ru);
+// } else if (lang === 'en') {
+//     console.log(en);
+// } else {
+//     console.log('Неверное значение переменной lang');
+// }
 
-const ru = 'Понедельник, вторник, среда, четверг, пятница, суббота, восскресенье';
-const en = 'Monday, tuesday, wensdey, thursday, friday, saturday';
+// const ru = 'Понедельник, вторник, среда, четверг, пятница, суббота, восскресенье';
+// const en = 'Monday, tuesday, wensdey, thursday, friday, saturday';
 
-let lang = 'en';
+// let lang = 'en';
 
-switch (lang) {
-    case 'ru':
-        console.log(ru);
-        break;
-    case 'en':
-        console.log(en);
-        break;
-    default:
-        console.log('Неверное значение переменной lang');
-}
+// switch (lang) {
+//     case 'ru':
+//         console.log(ru);
+//         break;
+//     case 'en':
+//         console.log(en);
+//         break;
+//     default:
+//         console.log('Неверное значение переменной lang');
+// }
+
+
+let lang = [
+    ['Понедельник, вторник, среда, четверг, пятница, суббота, восскресенье'],
+    ['Monday, tuesday, wensdey, thursday, friday, saturday']
+]
+console.log(lang[1]);
