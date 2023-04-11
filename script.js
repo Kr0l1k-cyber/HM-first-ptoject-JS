63abb7f136911b38679819691119ec8b33d18562
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-04-11 21:26:13 +0300

    решил задачу

--- script.js
+++ script.js
@@ -1,37 +1,18 @@
 'use strict';
 
-// const ru = 'Понедельник, вторник, среда, четверг, пятница, суббота, восскресенье';
-// const en = 'Monday, tuesday, wensdey, thursday, friday, saturday';
+const test = function(arg) {
+    if (typeof arg !== 'string') {
+        console.error('Ошибка: Аргумент не является строкой');
+        return;
+    } 
+    
+    arg = arg.trim()
 
-// let lang = 'ru';
+    if (arg.length > 30) {
+        return arg.slice(0, 30) + '...';
+      }
 
-// if (lang === 'ru') {
-//     console.log(ru);
-// } else if (lang === 'en') {
-//     console.log(en);
-// } else {
-//     console.log('Неверное значение переменной lang');
-// }
+    return arg
+}
 
-// const ru = 'Понедельник, вторник, среда, четверг, пятница, суббота, восскресенье';
-// const en = 'Monday, tuesday, wensdey, thursday, friday, saturday';
-
-// let lang = 'en';
-
-// switch (lang) {
-//     case 'ru':
-//         console.log(ru);
-//         break;
-//     case 'en':
-//         console.log(en);
-//         break;
-//     default:
-//         console.log('Неверное значение переменной lang');
-// }
-
-
-let lang = [
-    ['Понедельник, вторник, среда, четверг, пятница, суббота, восскресенье'],
-    ['Monday, tuesday, wensdey, thursday, friday, saturday']
-]
-console.log(lang[1]);
+console.log(test("Это длинная строка, которая содержит более 30 символов и будет обрезана"));
