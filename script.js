1ba167e9870098b8742aa62adde24e7b5d432ea5
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-05-04 21:54:42 +0300

    цикл перебора

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const arr = ['123', '246', '532', '444', '1235', '4783', '26789']
-console.log(arr[2]);
+
+for (let i = 0; i < arr.length; i++) {
+    const firstDigit = arr[i][0];
+    if (firstDigit === '2' || firstDigit === '4') {
+        console.log(arr[i]);
+    }
+    }
 
