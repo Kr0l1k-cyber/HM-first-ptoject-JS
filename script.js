a67fc7a5d2ada84167037b3873be55d6e9ee5cd2
Iyrii Pyshnov <coolspot64rus@gmail.com>
2023-05-04 22:08:54 +0300

    короче цикл перебора

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ for (let i = 0; i < arr.length; i++) {
     }
     }
 
+    for (let i = 2; i <= 100; i++) {
+        let isPrime = true;
+        
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+            isPrime = false;
+            break;
+            }
+        }
+        
+        if (isPrime) {
+            console.log(i, 'Делитель этого числа 1 и' + i);
+        }
+        }
